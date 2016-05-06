86e3b26b1aa39b240ca953522f076a9c44b6b59b
zongshuo <499832434@qq.com>
2016-05-06 17:01:20 +0800

    11

---
+++ app/src/main/java/com/sci99/scancode/zxing/myapplication2/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sci99.scancode.zxing.myapplication2.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/sci99/scancode/zxing/myapplication2/app/Person.java
@@ -0,0 +1,25 @@
+package com.sci99.scancode.zxing.myapplication2.app;
+
+/**
+ * Created by zongshuo on 2016/5/6.
+ */
+public class Person {
+    public String name;
+    public int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
